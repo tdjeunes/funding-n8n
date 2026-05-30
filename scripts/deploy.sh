cd614c991abb2303256d3d213ab36b6a6ab75183
Albert Albala <albert@mediatribe.net>
2026-05-30 13:21:51 -0400

    2026-05-30-13:21:50

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,14 @@ docker run -d \
   --env-file .env \
   -e N8N_BLOCK_ENV_ACCESS_IN_NODE=false \
   -e NODE_FUNCTION_ALLOW_EXTERNAL=mammoth \
+  -e NODE_FUNCTION_ALLOW_BUILTIN=crypto \
   --restart unless-stopped \
   n8n-custom
 
-echo "Visit http://localhost:5678 to access n8n (you might need to wait a minute or so). Use 'docker logs -f n8n' to see the logs."
+echo "Waiting for server to be available..."
+for i in $(seq 1 10); do
+  sleep 1
+  echo "$i/10"
+done
+
+echo "Visit http://localhost:5678 to access n8n. Use 'docker logs -f n8n' to see the logs."
